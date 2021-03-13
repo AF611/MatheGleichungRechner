cfc7d8266013e555c18a7066ee853ca59687eaf7
Alexanderf611 <alex2000.fritz@gmail.com>
2021-03-13 16:13:51 +0100

    Initial commit

---
+++ src/Gleichung.java
@@ -0,0 +1,120 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Gleichung {
+
+    private JPanel jPanel;
+    private JButton buttonRechnen;
+    private JButton buttonLoeschen;
+    private JLabel variableA;
+    private JLabel variableB;
+    private JLabel variableE;
+    private JLabel variableC;
+    private JLabel variableD;
+    private JLabel variableF;
+    private JTextField textFieldA;
+    private JTextField textFieldC;
+    private JTextField textFieldB;
+    private JTextField textFieldE;
+    private JTextField textFieldD;
+    private JTextField textFieldF;
+    private JLabel loesungY;
+    private JLabel loesungX;
+    private JLabel ergebnisX;
+    private JLabel ergebnisY;
+    private int a;
+    private int b;
+    private int c;
+    private int d;
+    private int e;
+    private int f;
+    private int x;
+    private int y;
+
+    public Gleichung(){
+        JFrame frame = new JFrame("Linearer Gleichungsrechner");
+        jPanel = new JPanel(new GridLayout(0, 2));
+        buttonRechnen = new JButton("Rechnen");
+        buttonLoeschen = new JButton("Löschen");
+        variableA = new JLabel("var: A");
+        variableB = new JLabel("var: B");
+        variableC = new JLabel("var: C");
+        variableD = new JLabel("var: D");
+        variableE = new JLabel("var: E");
+        variableF = new JLabel("var: F");
+        loesungX = new JLabel("Lösung X");
+        loesungY = new JLabel("Lösung Y");
+        ergebnisX = new JLabel("");
+        ergebnisY = new JLabel("");
+        textFieldA = new JTextField();
+        textFieldB  = new JTextField();
+        textFieldC  = new JTextField();
+        textFieldD  = new JTextField();
+        textFieldE  = new JTextField();
+        textFieldF  = new JTextField();
+
+
+        //Komponenten dem JPanel hinzufügen
+        frame.add(jPanel);
+        jPanel.add(variableA);
+        jPanel.add(textFieldA);
+        jPanel.add(variableB);
+        jPanel.add(textFieldB);
+        jPanel.add(variableC);
+        jPanel.add(textFieldC);
+        jPanel.add(variableD);
+        jPanel.add(textFieldD);
+        jPanel.add(variableE);
+        jPanel.add(textFieldE);
+        jPanel.add(variableF);
+        jPanel.add(textFieldF);
+        jPanel.add(buttonLoeschen);
+        jPanel.add(buttonRechnen);
+        jPanel.add(loesungX);
+        jPanel.add(loesungY);
+        jPanel.add(ergebnisX);
+        jPanel.add(ergebnisY);
+
+        buttonRechnen.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                gleichungRechnen();
+            }
+        });
+
+        buttonLoeschen.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                gleichungLoeschen();
+            }
+        });
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public void gleichungRechnen(){
+        //Wert für X berechnen
+        x = (e*d)-(f*b) / (d*a)-(c*b);
+        //Wert für Y berechnen
+        y = (f*a)-(c*e) / (d*a)-(c*b);
+        ergebnisX.setText("Ho");
+        ergebnisY.setText("Ich bins");
+    }
+
+    public void gleichungLoeschen(){
+        ergebnisX.setText("");
+        ergebnisY.setText("");
+    }
+
+
+
+    public static void main(String[] args){
+       new Gleichung();
+
+    }
+
+}
